72f911b7cc5fa58d7952f02ec831d87b73defe4d
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-03-26 12:12:09 +0100

    Ex1

---
+++ Ex1.java
@@ -0,0 +1,6 @@
+public class Ex1 {
+    int numero=0;
+while(numero < 10){
+    System.out.println(numero);
+    }
+}
---
+++ Ex1Test.java
@@ -0,0 +1,5 @@
+public class Ex1Test {
+    public static void main(String[] args) {
+
+    }
+}
